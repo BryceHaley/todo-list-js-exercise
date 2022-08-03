236e7cf705a9fb4f1c57ccfd4f7edb0d99bed736
Bryce Haley <bhaley91@gmail.com>
2022-08-03 12:57:57 -0300

    removed old functions

--- index.js
+++ index.js
@@ -20,18 +20,6 @@ function newTask(title, description) {
   };
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
-
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
